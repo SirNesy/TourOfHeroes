f47adf2ca100b438755443aaf859d3ab21007047
AzureAD\OnesiUmoize <onesi.umoize@gl-education.com>
2023-08-03 14:49:09 +0100

    this commit addshome  feature to the app by implementing router feature, also implements bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Tour of Heroes';
+  title: string = 'Tour of Heroes';
+  constructor(private router: Router) {}
+  home(): void {
+    this.router.navigate(['/dashboard']); // implementing the feature of router
+  }
 }
